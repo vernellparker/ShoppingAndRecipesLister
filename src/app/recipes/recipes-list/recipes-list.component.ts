3ef0ae39d7f84f618863eec89a6919d7a994ed68
Vernell Parker <vernell.parker@outlook.com>
2019-01-29 16:32:34 -0500

    Added Recipe container
     1  5 25

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,7 +7,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes:Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test.',
+      'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.media-allrecipes.com%2Fimages%2F56589.png&imgrefurl=https%3A%2F%2Fwww.allrecipes.com%2Frecipes%2F&docid=jSLnQsNp7iBGKM&tbnid=VhaiNYg1RVI4RM%3A&vet=10ahUKEwiEqpH_rZPgAhVKc98KHcQCCVEQMwhqKAEwAQ..i&w=300&h=300&bih=938&biw=1920&q=recipe&ved=0ahUKEwiEqpH_rZPgAhVKc98KHcQCCVEQMwhqKAEwAQ&iact=mrc&uact=8'
+    )
+  ];
 
   constructor() { }
 
